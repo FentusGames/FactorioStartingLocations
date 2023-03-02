9515aa038920acb0086d1636ae2ead520d91d2c8
FentusGames <124464575+FentusGames@users.noreply.github.com>
2023-03-02 11:56:17 -0500

    Add files via upload

---
+++ StartingLocations_2.0.8/freeplay.lua
@@ -0,0 +1,40 @@
+local freeplay_interface =
+{
+  get_created_items = function()
+    return global.created_items
+  end,
+  set_created_items = function(map)
+    global.created_items = map or error("Remote call parameter to freeplay set created items can't be nil.")
+  end,
+  get_respawn_items = function()
+    return global.respawn_items
+  end,
+  set_respawn_items = function(map)
+    global.respawn_items = map or error("Remote call parameter to freeplay set respawn items can't be nil.")
+  end,
+  set_skip_intro = function(bool)
+    global.skip_intro = bool
+  end,
+  set_chart_distance = function(value)
+    global.chart_distance = tonumber(value) or error("Remote call parameter to freeplay set chart distance must be a number")
+  end,
+  set_disable_crashsite = function(bool)
+    global.disable_crashsite = bool
+  end,
+  get_ship_items = function()
+    return global.crashed_ship_items
+  end,
+  set_ship_items = function(map)
+    global.crashed_ship_items = map or error("Remote call parameter to freeplay set created items can't be nil.")
+  end,
+  get_debris_items = function()
+    return global.crashed_debris_items
+  end,
+  set_debris_items = function(map)
+    global.crashed_debris_items = map or error("Remote call parameter to freeplay set respawn items can't be nil.")
+  end
+}
+
+if not remote.interfaces["freeplay"] then
+  remote.add_interface("freeplay", freeplay_interface)
+end
